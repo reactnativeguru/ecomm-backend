76943e7605e5d149b27059f1e1a92a40fced6465
John <john.amuesi@wipro.com>
2019-01-02 12:00:39 +0000

    created graphql yoga server for client side processing

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ecommerce Server App
 
-server
+## Packages
+
+1. Prisma: https://prisma.io/
+   to Deploy during dev: prisma deploy -e variables.env
+2. Prisma Bindings: https://github.com/prisma/prisma-binding
+   Connects our client to Prisma Graphql - middleware  
+3. GraphQl Yoga: https://github.com/prisma/graphql-yoga
+   Apollo , Express and more  for a feature rich GraphQL server
+   graphql-yoga is like create-react-app for building GraphQL servers. 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+// create our graphql server
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// express to handle cookies JWT
+
+//express to handle user auth
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+},
+ details => {
+        console.log(`Server running on port, 
+        http://localhost:${details.port}`)
+    }
+    );
